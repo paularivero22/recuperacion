0da89c1d45a3e2369ef3451b194d5d0f47cd9a4d
paularivero22 <paulariveroherreros789@gmail.com>
2025-05-20 15:17:37 +0200

    cambios

--- DWES/UT3_COMPOSER/Hoja03_COMPOSER_02/app/Interfaces/InterfazProveedorCorreo.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace App\Interfaces;
-
-    interface InterfazProveedorCorreo {
-        public function enviarCorreo(string $paraQuien, string $asunto, string $cuerpoMensaje): bool;
-    }
-?>
